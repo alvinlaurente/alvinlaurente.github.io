f583a7b43824e7e40f8736b830d594af64fc0511
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2022-07-16 02:36:46 +0700

    Playground & Button Component

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="w-screen h-screen bg-black font-bold text-center grid place-items-center">
-    <div class="text-white">
-      Hi, world!
+  <div class="w-screen h-screen font-bold text-center grid place-items-center">
+    <div>
+      <p class="font-ubuntu text-2xl mb-4 capitalize">
+        Hi, I'm <span class="font-noto text-primary">Alvin</span>! Enjoy the journey ahead.
+      </p>
+      <NuxtLink to="/playground" class="flex gap-2 items-center text-sm transition ease-in-out hover:text-primary">
+        <i class="fa fa-chevron-right" />
+        Playground
+      </NuxtLink>
     </div>
   </div>
 </template>
---
+++ pages/playground.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="w-screen h-screen font-bold text-center grid place-items-center">
+    <div>
+      <p class="font-ubuntu text-xl">
+        Buttons:
+      </p>
+      <div class="grid grid-cols-3 gap-4 mt-4 p-4 border-[1px] border-white">
+        <UniBtn>Primary</UniBtn>
+        <UniBtn outlined>
+          Primary_out
+        </UniBtn>
+        <UniBtn disabled>
+          Primary_dis
+        </UniBtn>
+        <UniBtn bg="success">
+          Success
+        </UniBtn>
+        <UniBtn bg="success" disabled>
+          Success_dis
+        </UniBtn>
+        <UniBtn bg="warning">
+          Warning
+        </UniBtn>
+        <UniBtn bg="warning" disabled>
+          Warning_dis
+        </UniBtn>
+        <UniBtn bg="danger">
+          Danger
+        </UniBtn>
+        <UniBtn bg="danger" disabled>
+          Danger_dis
+        </UniBtn>
+      </div>
+      <UniBtn to="/" class="mx-auto mt-4">
+        Back
+      </UniBtn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HiWorld',
+  data () {
+    return {
+      title: 'Hi World'
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
